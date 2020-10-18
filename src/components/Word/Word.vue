bceb5caf30799a300a1f292cdf14de98ccf2cc17
ArKhosrojerdi <khosrojerdi.alireza@gmail.com>
2020-10-18 16:18:58 +0330

    Fix word page height size

--- src/components/Word/Word.vue
+++ src/components/Word/Word.vue
@@ -381,12 +381,11 @@ export default {
   .main-page {
     border-radius: 0;
     box-shadow: none;
-    height: 100vh !important;
   }
 
   .game-settings {
-    border-radius: .5rem;
-    min-height: 100%;
+    border-radius: 0 0 .5rem .5rem;
+    min-height: calc(100vh - 30px) !important;
   }
 }
 
@@ -399,7 +398,7 @@ export default {
 
   .game-settings {
     border-radius: 0 0 .5rem .5rem;
-    min-height: calc(100vh - 8rem - 30px) !important;
+    min-height: calc(100vh - 30px) !important;
   }
 }
 
