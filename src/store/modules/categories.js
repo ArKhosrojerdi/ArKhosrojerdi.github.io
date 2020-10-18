c675830b13938dee6d7d6d9cba80e1a11c8be1bd
ArKhosrojerdi <khosrojerdi.alireza@gmail.com>
2020-10-18 15:16:16 +0330

    change dist file

--- src/store/modules/categories.js
+++
@@ -1,9 +0,0 @@
-import categories from '../../data/categories';
-
-const state = {
-    categories: categories
-};
-
-export default {
-    state
-};
--- src/store/modules/words.js
+++
@@ -1,9 +0,0 @@
-import words from '../../data/words';
-
-const state = {
-    words: words
-};
-
-export default {
-    state
-};
